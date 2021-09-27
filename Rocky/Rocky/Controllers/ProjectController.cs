c6744f523765905a90a1c15b72f418dc8b943d73
Константин Филюрин <rfrsk@yandex.ru>
2021-09-27 22:56:57 +0300

    Added create and edit buttons in form of Category

---
+++ Rocky/Rocky/Controllers/ProjectController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+    {
+    public class ProjectController : Controller
+        {
+        private readonly ApplicationDbContext _db;
+
+        public ProjectController(ApplicationDbContext db)
+            {
+            _db = db;
+            }
+
+        public IActionResult Index()
+            {
+            IEnumerable<Project> objList = _db.Project;
+            return View(objList);
+            }
+
+        //GET-Create
+        public IActionResult Create()
+            {
+            return View();
+            }
+
+        //Post-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create (Project obj)
+            {
+            _db.Add(obj);
+            _db.SaveChanges();
+            return View();
+            }
+
+        }
+    }
---
+++ Rocky/Rocky/Migrations/20210926174445_updateProject.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rocky.Migrations
+{
+    public partial class updateProject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Project",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Project");
+        }
+    }
+}
